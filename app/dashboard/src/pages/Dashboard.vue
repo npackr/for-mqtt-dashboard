9df780c71ccc6c9a9863e019ef320b1eff422c86
npackr <mateng@npackr.com>
2022-03-29 19:09:25 +0700

    Works on displaying data to UI

--- app/dashboard/src/pages/Dashboard.vue
+++ app/dashboard/src/pages/Dashboard.vue
@@ -167,24 +167,11 @@ import * as chartConfigs from "@/components/Charts/config";
 import TaskList from "./Dashboard/TaskList";
 import UserTable from "./Dashboard/UserTable";
 import config from "@/config";
-import gql from 'graphql-tag';
+import * as Realm from "realm-web";
 
-const BIG_CHART_QUERY = gql`query getBigChartQuery($offset: Int, $limit: Int) {
-      payload {
-        _id
-        _partition
-        payload
-        pdb
-        qos
-        timestamp
-        topic
-      },
-    }`
 
-cons
 
 export default {
-
   components: {
     LineChart,
     BarChart,
@@ -196,27 +183,14 @@ export default {
     return {
       bigLineChart: {
         allData: [
-          [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
+          bigLineChartDataset1,
           [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
           [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130],
         ],
         activeIndex: 0,
         chartData: {
           datasets: [{}],
-          labels: [
-            "JAN",
-            "FEB",
-            "MAR",
-            "APR",
-            "MAY",
-            "JUN",
-            "JUL",
-            "AUG",
-            "SEP",
-            "OCT",
-            "NOV",
-            "DEC",
-          ],
+          labels: payloads.timestamp,
         },
         extraOptions: chartConfigs.purpleChartOptions,
         gradientColors: config.colors.primaryGradient,
