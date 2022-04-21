b2b5cf23a1edeb17072ede82369c62cfd8ba28b0
npackr <mateng@npackr.com>
2022-04-21 21:30:09 +0700

    Add local and test connection type

--- client/client.js
+++ client/client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d957c3fd0a707c27ac67f105851a67a806e1278c091ce5df1dd180f9b3e26789
-size 3714
+oid sha256:0baf83c4f51acad4dc3883754d856d3c29e00f8d0872ca49d7e1461875316d1e
+size 4445
