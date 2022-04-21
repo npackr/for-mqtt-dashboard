e07605fca5df77ee331cc0e8f78668d9b3be5331
npackr <mateng@npackr.com>
2022-04-21 21:41:52 +0700

    Added some fixes

--- client/mqtt_modules/connector.js
+++ client/mqtt_modules/connector.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b198fafe7bf13ac85c648d3172a51baf1e7c4d16a0ee93cc2b487462cb680ed3
-size 2068
+oid sha256:100f2a4889125e21d3615f99162a223ac5d6bf8adfa5eee41958a38b910c3acf
+size 2047
