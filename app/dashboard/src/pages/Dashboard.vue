797ff7309939cb46b8baf8657c5f13bf59a4b43d
npackr <mateng@npackr.com>
2022-04-27 11:28:23 +0700

    Save the world

--- app/dashboard/src/pages/Dashboard.vue
+++ app/dashboard/src/pages/Dashboard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3713cde97a6f1be8e8aa898c515e5bbe1c4529737fdd0dbd1d382d83101405d
-size 11107
+oid sha256:8a52e0021423882fa9ffce05403ae3814727581e7a30dcca5b362f088a1cec38
+size 11379
--- app/old-dashboard/js/all.inis.js
+++ app/old-dashboard/js/all.inis.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7656d67945d15daf40d6f883e8f4c252356d33b83a4c3c22a2b3569020de3c6f
-size 8552
+oid sha256:449638c0cf493241edfb020254299b5ebe84f27a6775f8c3fb0a81d16e60c3d2
+size 8561
