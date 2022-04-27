d948e67cabd6e4634de4da1576dc031476721513
npackr <mateng@npackr.com>
2022-04-27 10:14:47 +0700

    Change connection type to cloud

--- client/client.js
+++ client/client.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0baf83c4f51acad4dc3883754d856d3c29e00f8d0872ca49d7e1461875316d1e
-size 4445
+oid sha256:b9d81c3786ba1af302295fafc8b57c93168785c63649f68bfdfd93e501465c1e
+size 4487
